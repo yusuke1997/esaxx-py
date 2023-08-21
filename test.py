0b3b428e54456dfec500c3945a4a2a8feee5e85c
yusuke1997 <63439062+yusuke1997@users.noreply.github.com>
2023-08-21 18:33:05 +0900

    add test code

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
 from io import StringIO
-from esa import esaxx
+from esa import esaxx, get_maximal_substrings
 
 class TestEsaxx(unittest.TestCase):
 
@@ -29,6 +29,38 @@ class TestEsaxx(unittest.TestCase):
         self.assertEqual(R, expected_R)
         self.assertEqual(D, expected_D)
         self.assertEqual(node_num, expected_node_num)
+
+
+class TestMaximalSubstrings(unittest.TestCase):
+
+    def test_get_maximal_substrings(self):
+        T = 'abracadabra'
+        substrings = get_maximal_substrings(T)
+
+        # 基本的なテスト: 戻り値がリストであること
+        self.assertIsInstance(substrings, list)
+
+        # すべてのsubstringがnamedtupleであることを確認
+        for substring in substrings:
+            self.assertTrue(hasattr(substring, 'count'))
+            self.assertTrue(hasattr(substring, 'length'))
+            self.assertTrue(hasattr(substring, 'string'))
+
+        # サンプル文字列'abracadabra'に基づく具体的なテスト
+        # この部分は、期待される結果に基づいています。実際の結果に合わせて調整してください。
+        expected_substrings = [
+            # (count, length, string)
+            # 以下は例です。実際の期待される結果に合わせて変更してください。
+            (2, 4, 'abra'),
+            (5, 1, 'a')
+            # ... 他の期待されるsubstring
+        ]
+
+        for expected, actual in zip(expected_substrings, substrings):
+            self.assertEqual(expected[0], actual.count)
+            self.assertEqual(expected[1], actual.length)
+            self.assertEqual(expected[2], actual.string)
+
 
 
 if __name__ == '__main__':
